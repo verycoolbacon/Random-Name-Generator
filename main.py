63703dff4dfe349eb2fcada7b751b81a51ba9383
verycoolbacon <kao991203@gmail.com>
2026-09-22 22:04:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 import string
+
+# main function start
+
 def letter_gen(amount):
     func_list = string.ascii_lowercase
     temp = []
@@ -35,16 +38,33 @@ def symbol_gen(amount):
         temp.append(random.choice(func_list))
     result = "".join(temp)
     return result
+
+# main function end
+
+# main asking script start
+
 letter_gen_input = input("Enter amount of letters. R for random: ")
+while True:
+    try:
+        letter_gen_output = letter_gen(letter_gen_input)
+        break
+    except ValueError:
+        letter_gen_input = input("Please enter any number or R for amounts of letters: ")
+
 num_gen_input = input("Enter amount of numbers. R for random: ")
-symbol_gen_input = input("Enter amount of symbols. R for random: ")
+while True:
+    try:
+        num_gen_output = num_gen(num_gen_input)
+        break
+    except ValueError:
+        num_gen_input = input("Please enter any number or R for amounts of numbers: ")
 
+random_upper_choice = input("Randomize upper charaters? [y/n] ")
+symbol_gen_choice = input("Generate random symbols? [y/n] ")
 
-letter_gen_output = letter_gen(letter_gen_input)
-num_gen_output = num_gen(num_gen_input)
-symbol_gen_output = symbol_gen(symbol_gen_input)
+# main asking script end
 
-random_upper_choice = input("randomize upper charaters? [y/n] ")
+# random uppercase choice
 
 if random_upper_choice in ("y","Y"):
     temp_letters = []
@@ -59,4 +79,19 @@ if random_upper_choice in ("y","Y"):
 else:
     randomized_upper = letter_gen_output
 
+# symbol generator choice
+
+if symbol_gen_choice in ("y","Y"):
+    symbol_gen_input = input("Enter amount of symbols. R for random: ")
+    while True:
+        try:
+            symbol_gen_output = symbol_gen(symbol_gen_input)
+            break
+        except ValueError:
+            symbol_gen_input = input("Please enter any number or R for amounts of symbols: ")
+else:
+    symbol_gen_output = ""
+
+# final answer output
 print(num_gen_output+randomized_upper+symbol_gen_output)
+# this script is made by verycoolbacon
